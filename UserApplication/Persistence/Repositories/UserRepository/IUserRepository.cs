3ddc9b4d6f7706db33d50c25503a368889c7cc34
krino45 <krina645@gmail.com>
2025-05-17 17:11:22 +0700

    Finish simple DAO layer

---
+++ UserApplication/Persistence/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using UserApplication.API.Models;
+
+namespace UserApplication.Persistence.Repositories.UserRepository
+{
+    public interface IUserRepository
+    {
+        public Task<List<User>> GetAllAsync();
+        public Task<User?> GetByIdAsync(Guid id);
+        public Task<User?> GetByLoginAsync(string login);
+        public Task AddAsync(User user);
+        public Task UpdateAsync(User user);
+        public Task DeleteAsync(Guid id);
+    }
+}
---
+++ UserApplication/Persistence/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserApplication.API.Models;
+
+namespace UserApplication.Persistence.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        private AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<User>> GetAllAsync() =>
+            await _context.Users.ToListAsync();
+        public async Task<User?> GetByIdAsync(Guid id) =>
+            await _context.Users.FindAsync(id);
+        public async Task<User?> GetByLoginAsync(string login) =>
+            await _context.Users.FirstOrDefaultAsync(w => w.Login == login);
+        public async Task AddAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(User user)
+        {
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(Guid id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
